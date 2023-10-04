fb296b8e9c7553233b56b8a0ca518eff35a932f8
maneesh <plammaneesh@gmail.com>
2023-10-04 16:27:23 +0530

    react all files add
    add .gitgnore, package-lock.json, package.json, README.md

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBc9PH7g9Ere9k0IFqXxfcfZNv0pkczFXk",
+    authDomain: "clone-fbe78.firebaseapp.com",
+    projectId: "clone-fbe78",
+    storageBucket: "clone-fbe78.appspot.com",
+    messagingSenderId: "464372947953",
+    appId: "1:464372947953:web:e59e01e08b21dd5068c250"
+  };
